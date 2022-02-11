f094aabdf8a49a8b4f0e57137ed79246db17b5a2
vadobry <vadobry@icloud.com>
2022-02-12 00:10:55 +0500

    Stage 2 - virtual rendering and exit on press esc - works for now but many gliches

--- main.lua
+++ main.lua
@@ -1,32 +1,49 @@
 --[[
     Your love2d game start here
 ]]
+-- push  - library for start game in virtual resolution- more retro game aesthetic
+local push = require 'push'
 WINDOW_WIDTH = 1280
-WINDOW_HEIGHT = 720
+WINDOW_HEIGHT = 800
 
---love.graphics.setDefaultFilter('nearest', 'nearest')
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+local settings= {
+    fullscreen = false,
+    resizable = false,
+    vsync = true
+} 
+
+
 
 function love.load()
     -- init something here ...
     --initialising game state 
-    love.window.setMode(WINDOW_WIDTH,WINDOW_HEIGHT,{
-        fullscreen = false,
-        resizable = false,
-        vsync = true
-    })
+    -- love.window.setMode(WINDOW_WIDTH,WINDOW_HEIGHT,{
+    --     fullscreen = false,
+    --     resizable = false,
+    --     vsync = true
+    -- })
+    
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, settings)
+  
 end
+
+
+ 
 
 -- function love.resize(w, h)
 --     -- ...
 -- end
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
 
--- function love.keypressed(key)
---     if key == 'escape' then
---         love.event.quit()
---     end
-
---     love.keyboard.keysPressed[key] = true
--- end
+    love.keyboard.keysPressed[key] = true
+end
 
 -- function love.keyboard.wasPressed(key)
 --     return love.keyboard.keysPressed[key]
@@ -40,6 +57,9 @@ end
 
 function love.draw()
     -- draw your stuff here
-    love.graphics.printf('Hello Pong!', 0, WINDOW_HEIGHT/2-6,WINDOW_WIDTH,'center')
+    push:apply("start")
+    love.graphics.printf('Hello Pong!',0,VIRTUAL_HEIGHT / 2 -6, VIRTUAL_WIDTH,'center')
+
+    push:apply("end")
 end
 
